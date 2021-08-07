86902e7d25c3f2d9f222872bd6be46d072381b38
Bhavya Tummala <bhavya@Bhavyas-MBP.fios-router.home>
2021-08-07 00:27:32 -0400

    deleted files

--- Day1Project_BandName.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to the Band Name generator")
-City_Name = input("What's the name of the city you grew up in?\n")
-Pet_Name = input("What's the name of your pet?\n")
-print("Your band name could be "+City_Name+' '+Pet_Name)
-                 
